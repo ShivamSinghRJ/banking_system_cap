845b3c34c9bb5a4e6ea82d54f7827ef505d2130f
Shivam Kumar <shivam.t.kumar@capgemini.com>
2025-09-03 17:06:07 +0000

    Your commit message

--- app/bank_system/webapp/controller/View7.controller.js
+++ app/bank_system/webapp/controller/View7.controller.js
@@ -1,11 +1,75 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller,JSONModel) => {
     "use strict";
+    var mail;
+    var data;
 
     return Controller.extend("banksystem.controller.View7", {
         onInit() {
+             const oModel = new sap.ui.model.odata.v4.ODataModel({
+                serviceUrl: "/admin/"
+              });
+              this.getView().setModel(oModel);
+
+              this.getOwnerComponent().getRouter()
+            .getRoute("RouteView7")
+            .attachPatternMatched(this._onRouteMatched, this);
         },
+
+         _onRouteMatched: function (oEvent) {
+            mail = oEvent.getParameter("arguments").email;
+           
+             var oModel = this.getView().getModel(); // OData V4 model
+            var oBinding = oModel.bindList("/People");
+            oBinding.requestContexts(0, 100).then(function (aContexts) {
+               var aAllData = aContexts.map(function (oContext) {
+                   return oContext.getObject();
+               });
+
+               var aFiltered = aAllData.filter(function (entry) {
+                return entry.email === mail;
+              });
+              data = aFiltered;
+              debugger;
+              
+              var initial = data[0].First_name.charAt(0) + data[0].Last_name.charAt(0);
+              this.getView().byId("_IDGenAvatar5").setInitials(initial);
+
+            // If _IDGenTitle21 is a Text or Label control
+            this.getView().byId("_IDGenText51").setText(mail);
+            this.getView().byId("_IDGenTitle21").setText(data[0].name);
+            this.getView().byId("_IDGenInput16").setValue(mail);
+             this.getView().byId("_IDGenInput17").setValue(data[0].name);
+            //setValue
+             
+         }.bind(this)).catch(function (oError) {
+              console.error("Error loading data:", oError);
+         });
+
+
+         },
+
+         addBank:function(){
+            var bankAccNo = this.getView().byId("_IDGenInput18").getValue();
+            var expDate = this.getView().byId("_IDGenInput19").getDateValue().toLocaleDateString('en-CA');
+            var bankName = this.getView().byId("_IDGenInput20").getValue();
+
+
+            const newEntry = {
+                email : mail,
+                bank_name : bankName,
+                card_number : bankAccNo,
+                expire_date : expDate,
+                amount : 0
+            };
+
+            var oModel = this.getView().getModel();
+            const oCreateBinding = oModel.bindList("/Bank");
+                oCreateBinding.create(newEntry);
+            sap.m.MessageToast.show("Entry created successfully");
+         },
          home:function(){
            this.getOwnerComponent().getRouter().navTo("RouteView3",{
                 email : mail
--- app/bank_system/webapp/controller/View8.controller.js
+++ app/bank_system/webapp/controller/View8.controller.js
@@ -1,13 +1,147 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox"
+], (Controller,JSONModel,MessageBox) => {
     "use strict";
+    var mail;
+    var data,bankData;
 
     return Controller.extend("banksystem.controller.View8", {
         onInit() {
-           
-              
+             const oModel = new sap.ui.model.odata.v4.ODataModel({
+                serviceUrl: "/admin/"
+              });
+              this.getView().setModel(oModel);
+
+              this.getOwnerComponent().getRouter()
+            .getRoute("RouteView8")
+            .attachPatternMatched(this._onRouteMatched, this);
+
+            this.bankDetails();
         },
+
+          _onRouteMatched: function (oEvent) {
+            mail = oEvent.getParameter("arguments").email;
+           
+             var oModel = this.getView().getModel(); // OData V4 model
+            var oBinding = oModel.bindList("/People");
+            oBinding.requestContexts(0, 100).then(function (aContexts) {
+               var aAllData = aContexts.map(function (oContext) {
+                   return oContext.getObject();
+               });
+
+               var aFiltered = aAllData.filter(function (entry) {
+                return entry.email === mail;
+              });
+              data = aFiltered;
+            //   debugger;
+              
+              var initial = data[0].First_name.charAt(0) + data[0].Last_name.charAt(0);
+              this.getView().byId("_IDGenAvatar6").setInitials(initial);
+
+            // If _IDGenTitle21 is a Text or Label control
+            this.getView().byId("_IDGenText53").setText(mail);
+            this.getView().byId("_IDGenTitle24").setText(data[0].name);
+            this.getView().byId("_IDGenInput21").setValue(mail);
+            //  this.getView().byId("_IDGenInput17").setValue(data[0].name);
+            //setValue
+            // debugger;
+             
+         }.bind(this)).catch(function (oError) {
+              console.error("Error loading data:", oError);
+         });
+        },
+
+        bankDetails:function(){
+            var oModel = this.getView().getModel();
+            var oBinding = oModel.bindList("/Bank");
+            oBinding.requestContexts(0, 100).then(function (aContexts) {
+               var aAllData = aContexts.map(function (oContext) {
+                   return oContext.getObject();
+               });
+               bankData = aAllData;
+
+               var aFiltered = aAllData.filter(function (entry) {
+                return entry.email === mail;
+              });
+            
+              var oJSONModel = new JSONModel(aFiltered);
+              this.getView().setModel(oJSONModel, "BankModel");
+            }.bind(this)).catch(function (oError) {
+                console.error("Error loading data:", oError);
+           });
+        },
+
+
+
+        AddMonaeyToBank:function(){
+           
+            try{
+
+                var bank =  this.byId("_IDGenComboBox3").getSelectedItem().getText();
+                var msg = this.getView().byId("_IDGenTextArea1").getValue();
+                var accNumber =  this.byId("_IDGenComboBox6").getSelectedItem().getText();
+                var amount = this.getView().byId("_IDGenInput23").getValue();
+
+                if(bank === undefined || accNumber == undefined || amount == undefined){
+                    debugger;
+                    MessageBox.warning("Fill all data!!!");
+                    return;
+                }
+                debugger;
+            }catch(err){
+                
+            }
+           debugger;
+
+            var FilteredData = bankData.filter(function (entry) {
+                return entry.email === mail && entry.bank_name === bank && entry.card_number === accNumber;
+              });
+            
+            if(FilteredData.length > 0){
+                var oModel = this.getView().getModel();
+
+                 const oNewEntry =  {
+                          email : mail,
+                          bank_name : bank,
+                          card_number : accNumber,
+                          transaction : "Money Added",
+                          amount : parseFloat(amount),
+                          status : "Success",
+                          category : "Self",
+                          message : msg,
+                          recipant_bank_number : accNumber
+                      };
+                const oCreateBinding = oModel.bindList("/Transaction");
+                        oCreateBinding.create(oNewEntry);
+                        sap.m.MessageToast.show("Money Added successfully");
+
+                const sId = encodeURIComponent(FilteredData[0].ID);
+                 const sPath = `/Bank(ID='${sId}')`;
+                const oContextBinding = oModel.bindContext(sPath);
+                const oContext = oContextBinding.getBoundContext();
+
+                 // Safety check
+                if (!oContext) {
+                  MessageToast.show("No context found for update.");
+                  return;
+                }
+                debugger;
+                oContext.setProperty("amount", parseFloat(FilteredData[0].amount) + parseFloat(amount));
+                
+
+            }else{
+                	// MessageBox.warning("A profile with this email already exists.");
+                         var msg4 = this.getView().byId("_IDGenMessageStrip10").setVisible(true);
+                            setTimeout(function() {
+                                msg4.setVisible(false);
+                            }, 2000);
+
+            }
+
+        },
+
         home:function(){
            this.getOwnerComponent().getRouter().navTo("RouteView3",{
                 email : mail
